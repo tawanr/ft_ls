a6f0d9bb79e9ab917645642d76bbe57e4d5942f0
tratanat <tawan.rtn@gmail.com>
2025-06-11 08:08:57 +0700

    Refactor tests

--- tests/test_ls.sh
+++ tests/test_ls.sh
@@ -3,7 +3,7 @@
 # Colors for output
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 # Function to compare outputs
 compare_outputs() {
@@ -39,26 +39,26 @@ compare_outputs() {
 
 # Create test directory structure
 mkdir -p test_dir
-sleep 1
-current_time=$(date +%Y%m%d%H%M)
+current_time=$(date --date="2025-06-10 10:00:00" +%Y%m%d%H%M)
+current_time=$((current_time + 1))
+dir_list=("dir1" "dir2" "dir1/dir3" "dir1/dir3/dir4" "dir1/dir3/dir4/dir5")
+file_list=("file1" "file2" "file3" "dir1/dir3/dir4/dir5/file6" "dir1/dir3/dir4/file7" "dir1/dir3/file8" "dir1/file4" "dir2/file5")
 cd test_dir
-for filename in "file1" "file2" "file3"; do
-    # touch $filename
-    # sleep 1
-    touch -t $current_time $filename
-    current_time=$((current_time - 2))
-done
 chmod +x file1
-for dirname in "dir1" "dir2" "dir1/dir3" "dir1/dir3/dir4" "dir1/dir3/dir4/dir5"; do
+for dirname in "${dir_list[@]}"; do
     mkdir -p $dirname
-    sleep 1
 done
-for filename in "dir1/dir3/dir4/dir5/file6" "dir1/dir3/dir4/file7" "dir1/dir3/file8" "dir1/file4" "dir2/file5" ; do
-    touch $filename
-    sleep 1
+for filename in "${file_list[@]}"; do
+    touch -t $current_time $filename
+    current_time=$((current_time + 1))
+done
+for dirname in "${dir_list[@]}"; do
+    touch -t $current_time $dirname
+    current_time=$((current_time + 1))
 done
 touch -t 202402010830 old_file
 ln -s file1 symlink1
+touch -t $current_time symlink1
 cd ..
 
 # Basic tests
